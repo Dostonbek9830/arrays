bb35a10692aac338595cefa66f0f7783c3e25d33
Dostonbek Yoqubov <dostonbek.yoqubov9830@gmail.com>
2023-07-30 23:58:38 +0500

    updated

---
+++ main.js
@@ -0,0 +1,17 @@
+//Calling arrays
+let fruits = ["apple", "banana", "cherry", "apricot", "peach"];
+
+// console.log(fruits);
+
+//Change data of inside array
+// fruits[0] = "strawberry";
+// console.log(fruits);
+
+//Addition two arrays
+
+let vegetables = ["onion", "potatoes", "carrot"]
+let concat = fruits.concat(vegetables);
+console.log(concat);
+
+
+//
